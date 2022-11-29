ec62b8f59902107f8a1a894a8724c7cae1458d14
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-11-29 12:43:06 +0100

    Challenge quête 5 terminé - Ajout route pour programmes

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -16,4 +16,13 @@ class ProgramController extends AbstractController
             'website' => 'Wild Series',
         ]);
     }
+
+
+    #[Route('/program/{id}', methods: ['GET'], requirements: ['id' => '\d+'], name: 'program_show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+    }
 }
