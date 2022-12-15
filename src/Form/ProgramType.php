05b69ee3508fd1e229fd741c1f7334eaed4d09c7
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2022-12-15 12:49:05 +0100

    Challenge quête 22 terminé - Ajout posters + images pour actor et affichage

--- src/Form/ProgramType.php
+++ src/Form/ProgramType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Entity\Actor;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class ProgramType extends AbstractType
 {
@@ -16,7 +17,11 @@ class ProgramType extends AbstractType
         $builder
             ->add('title')
             ->add('synopsis')
-            ->add('poster')
+            ->add('posterFile', VichFileType::class, [
+                'required'      => false,
+                'allow_delete'  => true, // not mandatory, default is true
+                'download_uri' => true, // not mandatory, default is true
+            ])
             ->add('category', null, ['choice_label' => 'name'])
             ->add('actors', EntityType::class, [
                 'class' => Actor::class,
